304d500bf819c09a56d7d2bfe17c577adc6845ef
Gavin <me@gavin.hk>
2017-05-17 14:17:44 +0800

    Update vila path for install script

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-vila=~/vila
+$VILA=$HOME/.vila
 
 # Install vila
-git clone https://github.com/gavinkflam/vila.git ~/vila
+git clone https://github.com/gavinkflam/vila.git ~/.vila
 
 # Install on-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
