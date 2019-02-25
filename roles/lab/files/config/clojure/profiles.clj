3893de3d18593e1e026f7379a2b20bff6bd4aa92
Gavin Lam <me@gavin.hk>
2019-02-25 23:14:15 +0800

    Added leiningen installation task for clojure

---
+++ roles/lab/files/config/clojure/profiles.clj
@@ -0,0 +1,4 @@
+{:user
+ {:plugins [[cider/cider-nrepl "0.21.1"]]
+  :dependencies [[cljfmt "0.6.4" :exclusions [org.clojure/clojure rewrite-cljs]]
+                 [jonase/kibit "0.1.6" :exclusions [org.clojure/clojure]]]}}
